79a58c3ec9404138bfb34169812599f3f03c516a
Your Name <you@example.com>
2026-04-21 13:49:28 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ai-ekonomi/sw.js', { scope: '/ai-ekonomi/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
